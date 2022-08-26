cc76ad6b1a82d6e423cf64f3b378894abb2e51ad
Lorenzo Mendoza <enzo052095@gmail.com>
2022-08-26 11:40:56 -0400

    ntl test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import{MoralisProvider} from "react-moralis"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MoralisProvider serverUrl={process.env.NEXT_PUBLIC_SERVER}
-    appId={process.env.NEXT_PUBLIC_APP_ID}>
+    <MoralisProvider serverUrl={process.env.NEXT_PUBLIC_SERVER!}
+    appId={process.env.NEXT_PUBLIC_APP_ID!}>
       <Component {...pageProps} />
     </MoralisProvider>
   )
